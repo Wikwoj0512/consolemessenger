431141055ecc0861df331560f05e4c765db7f094
Wikwoj0512 <wiktor.wojtkowiak@gmail.com>
2019-10-10 14:42:51 +0200

    showing replies, scroll ing

--- official_v1.py
+++ official_v1.py
@@ -9,6 +9,7 @@ ERASE_LINE = '\x1b[2K'
 threadslast=[]
 threadsupdate=[]
 end=False
+height=0
 def exitcode():
     global end
     end=True
@@ -29,11 +30,17 @@ def collect():
         else:
             writing.append(text)
 def send():
-    global last, writing, current, threadusers,messages
+    global last, writing, current, threadusers,messages,height, threadsupdate
     text="".join(writing)
     writing=[]
     if text=="^end": exitcode()
-    if text=="^exit":current,writing=0,[]
+    if text=="^exit":current,writing,height=0,[],0
+    if text=="^up" and current!=0: height+=1
+    if text == "^dn" and current != 0 and height>0:
+        height -= 1
+        if height==0:
+            if current.name in threadsupdate: threadsupdate.remove(current.name)
+
     if text[0]=='^':
         if len(text[1:])==16:
             try:
@@ -46,8 +53,9 @@ def send():
                     for user in client.fetchAllUsersFromThreads([current]):
                         threadusers[user.uid]=user.name
                 threadusers[client.uid] = "You"
-                messages = client.fetchThreadMessages(thread_id=current.uid, limit=25)
-                messages.reverse()
+                messages = client.fetchThreadMessages(thread_id=current.uid,  limit=25*(height+1))[height*25:]
+                # messages.reverse()
+                height=0
                 print("dolaczono")
             except:
                 print("nie znaleziono wątku")
@@ -62,8 +70,8 @@ def send():
                     for user in client.fetchAllUsersFromThreads([current]):
                         threadusers[user.uid]=user.name
                 threadusers[client.uid]="You"
-                messages = client.fetchThreadMessages(thread_id=current.uid, limit=25)
-                messages.reverse()
+                messages = client.fetchThreadMessages(thread_id=current.uid, limit=25*(height+1))[height*25:]
+                height=0
                 print("dolaczono")
 
             except:
@@ -80,35 +88,36 @@ def send():
                 pass
 
 def onmessage(message_object, author_id, thread_id, thread_type):
-    global last,marker, threadsupdate, messages, threadslast, messageslast
+    global last,marker, threadsupdate, messages, threadslast, messageslast, height
     threadslast = client.fetchThreadList(limit=20)
     thread=client.fetchThreadInfo(thread_id)[thread_id]
     if messageslast:
         messageslast[thread.uid] = message_object.text
-    if current.name in threadsupdate: threadsupdate.remove(current.name)
-    if thread.name not in threadsupdate and thread_id!=current.uid:
+    if current.name in threadsupdate and height<1: threadsupdate.remove(current.name)
+    if( thread.name not in threadsupdate and thread_id!=current.uid) or height>0:
         threadsupdate.append(thread.name)
     if current!=0:
         if current.uid==thread_id:
-            if not messages:
-                if message_object:
-                    messages.append(message_object)
-                    messages=messages[1:]
+            if messages:
+                message = client.fetchThreadMessages(thread_id=thread_id, limit=25 * (height + 1))[height * 25] if height >0 else message_object
+                messages.insert(0,message)
+                messages=messages[:-1]
             else:
-                messages = client.fetchThreadMessages(thread_id=thread_id, limit=25)
-                messages.reverse()
+                messages = client.fetchThreadMessages(thread_id=thread_id, limit=25*(height+1))[height*25:]
+
 
 class CustomClient(Client):
     def onMessage(self, message_object, author_id, thread_id, thread_type, **kwargs):
         onmessage(message_object, author_id, thread_id, thread_type)
 
-def printer():
-    global writing, current, client, threadsupdate,end, messages,threadusers,threadslast, messageslast
 
+def printer():
+    global writing, current, client, threadsupdate,end, messages,threadusers,threadslast, messageslast, height
     while not end:
         try:
             print(" ".join( threadsupdate))
-            if current==0:
+            if current == 0:
+                height=0
                 messages=False
                 threadsupdate=[]
                 if not threadslast:
@@ -124,12 +133,12 @@ def printer():
                     i+=1
             else:
                 thread=current
-                print(" "*20+thread.name)
+                print(" "*20+thread.name, height)
                 if not messages:
-                    messages = client.fetchThreadMessages(thread_id=thread.uid, limit=25)
-                    messages.reverse()
-                for message in messages:
-                    print(f"\n{threadusers[message.author]}: {message.text}")
+                    messages = client.fetchThreadMessages(thread_id=thread.uid,limit=25*(height+1))[height*25:]
+                for index, message in list(enumerate(messages))[::-1]:
+                    if message.replied_to: print(f"\n[{index+(height*25)}] (reply to: {message.replied_to.text}): {threadusers[message.author]}: {message.text}")
+                    else: print(f"\n[{index+(height*25)}] {threadusers[message.author]}: {message.text}")
             print("write: ","".join(writing))
             if current!=0:
                 i=0
@@ -141,12 +150,13 @@ def printer():
         except Exception as e:
             print(e)
 
-username =input("nazwa użytkownika: ")
-passwd=input('hasło')
+
+username = input("nazwa użytkownika: ")
+passwd = input('hasło')
 client = CustomClient(username, passwd)
 t = threading.Thread(target=client.listen)
 t.start()
-t2=threading.Thread(target=collect)
+t2 = threading.Thread(target=collect)
 t2.start()
-t3=threading.Thread(target=printer)
+t3 = threading.Thread(target=printer)
 t3.start()
